7dac141cc4110cc7a40dfad3619e4500acb90d6a
mgr_inz_rafal <rchabowski@gmail.com>
2021-12-04 14:30:41 +0100

    Slight refactor

--- tools/rotate_precalc/src/main.rs
+++ tools/rotate_precalc/src/main.rs
@@ -2,6 +2,8 @@ use std::{fmt::Display, fs::File, io::Write, thread, time::Duration};
 
 use crossterm::{cursor, style, terminal, ExecutableCommand};
 
+use itertools::Itertools;
+
 const MID_POINT: f64 = 5.5;
 
 fn draw<'a, I, S>(map: I, starting_line: usize)
@@ -23,6 +25,8 @@ where
 fn get(map: &[&str], x: f64, y: f64) -> Option<char> {
     let ix = x.round() as i32;
     let iy = y.round() as i32;
+
+    
     if ix < 0 || ix > 11 || iy < 0 || iy > 11 {
         return None;
     };
@@ -38,30 +42,33 @@ fn get(map: &[&str], x: f64, y: f64) -> Option<char> {
 fn rotate(map: &[&str], angle: f64, rotation: &mut RotateDef) -> Vec<String> {
     let angle = angle.to_radians();
 
-    let mut new_map = Vec::with_capacity(12);
-    for y in 0..=11 {
-        let mut row = Vec::with_capacity(12);
-        for x in 0..=11 {
-            let rx = angle.cos() * (x as f64 - MID_POINT) - angle.sin() * (y as f64 - MID_POINT)
-                + MID_POINT;
-            let ry = angle.sin() * (x as f64 - MID_POINT)
-                + angle.cos() * (y as f64 - MID_POINT)
-                + MID_POINT;
+    (0..=11)
+        .map(|y| {
+            (0..=11)
+                .map(|x| {
+                    let rx = angle.cos() * (x as f64 - MID_POINT)
+                        - angle.sin() * (y as f64 - MID_POINT)
+                        + MID_POINT;
+                    let ry = angle.sin() * (x as f64 - MID_POINT)
+                        + angle.cos() * (y as f64 - MID_POINT)
+                        + MID_POINT;
 
-            let maybe_tile = get(&map, rx, ry);
-            if maybe_tile.is_some() {
-                rotation.from.push((rx.round() as i8, ry.round() as i8));
-                rotation.to.push((x as i8, y as i8));
-            } else {
-                rotation.empty.push((x as i8, y as i8));
-            }
+                    get(&map, rx, ry).map_or_else(
+                        || {
+                            rotation.empty.push((x as i8, y as i8));
+                            ' '
+                        },
+                        |new_tile| {
+                            rotation.from.push((rx.round() as i8, ry.round() as i8));
+                            rotation.to.push((x as i8, y as i8));
 
-            let new_tile = maybe_tile.unwrap_or(' ');
-            row.push(new_tile);
-        }
-        new_map.push(row.iter().collect::<String>());
-    }
-    new_map
+                            new_tile
+                        },
+                    )
+                })
+                .collect::<String>()
+        })
+        .collect_vec()
 }
 
 #[derive(Default)]
@@ -116,8 +123,6 @@ fn main() {
     for i in (10..=90).step_by(10) {
         let mut stdout = std::io::stdout();
         stdout
-            .execute(cursor::Hide)
-            .unwrap()
             .execute(terminal::Clear(terminal::ClearType::All))
             .unwrap()
             .execute(cursor::MoveTo(20, 0))

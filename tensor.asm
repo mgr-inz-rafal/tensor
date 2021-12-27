6b9f722bd7d9c56414cc4f066cd00c63eed460af
mgr_inz_rafal <rchabowski@gmail.com>
2021-12-27 21:19:58 +0100

    Add place for english level names and reorganize memory

--- tensor.asm
+++ tensor.asm
@@ -321,6 +321,132 @@ TITLE_BOTTOM_BORDER
 :37	dta b(125)
 	dta b(93)
 	dta b(93)
+MENU_0_DATA
+	dta d'               Graj                     '
+	dta d'                                        '
+	dta d'               Opcje                    '
+	dta d'                                        '
+	dta d'               Instrukcja               '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'               Wyjscie                  '
+LUDEK_DATA
+		dta b($62)
+		dta b($16)
+		dta b($38)
+		dta b($18)
+		dta b($70)
+		dta b($30)
+		dta b($70)
+		dta b($00)
+		
+		dta b($30)
+		dta b($1C)
+		dta b($18)
+		dta b($18)
+		dta b($10)
+		dta b($38)
+		dta b($18)
+		dta b($38)
+		
+		dta b($18)
+		dta b($18)
+		dta b($1C)
+		dta b($18)
+		dta b($10)
+		dta b($38)
+		dta b($18)
+		dta b($38)
+
+		dta b($0C)
+		dta b($30)
+		dta b($18)
+		dta b($18)
+		dta b($10)
+		dta b($38)
+		dta b($18)
+		dta b($38)
+
+AMYGDALA_DATA_5	; Miecz
+	dta b(0),b(192),b(160),b(84),b(44),b(24),b(52),b(2),b($64),b($74)
+
+AMYGDALA_DATA_6	; Pierscionek
+	dta b(0),b(60),b(24),b(52),b(82),b(64),b(66),b(52),b($a6),b($b6)
+
+AMYGDALA_DATA_7	; Robak
+	dta b(0),b(146),b(130),b(84),b(16),b(88),b(16),b(56),b($34),b($44)
+
+scr_head     .he 00 00 00 00 00 00 00 c1 00 00 00 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 42 00 03 04 05 06 87 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 00 08 09 0a 0b 0c 00 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 4d 00 0e 0f 10 d1 d2 00 93 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 00 00 d4 15 d4 00 00 00 00 00 00 00 00 00 00 00
+ 	.he 00 00 00 00 00 16 17 18 99 9a 00 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 00 00 1b 5c 9d 00 00 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 00 00 1e 1f a0 00 00 00 00 00 00 00 00 00 00 00
+	.he 00 00 00 61+128 62+128 63+128 00+128 64+128 65+128 66+128 67+128 68+128 00 00 00 00 00 00 00 00
+	.he 00 00 00 00 69 6a 6b 6c 6d 6e 6f 79+128 00 00 00 00 00 00 00 00
+	.he 00 00 00 70+128 71+128 72+128 73+128 74+128 75+128 76+128 77+128 78+128 00 00 00 00 00 00 00 00
+
+MENU_0_DATA_EN
+	dta d'               Play                     '
+	dta d'                                        '
+	dta d'               Options                  '
+	dta d'                                        '
+	dta d'               Info                     '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'               Exit                     '
+
+MENU_1_DATA
+	dta d'      Przyspieszenie grawitacyjne:      '
+	dta d'           '
+GRAVITY_LABEL
+	dta d'     POTEZNE      '*
+	dta d'           '
+	dta d'                                        '
+	dta d'            Obrot pieczary:             '
+	dta d'               '
+ROTATION_LABEL
+	dta d' WLACZONY'
+	dta d'                '
+	dta d'                                        '
+	dta d'                 Jezyk:                 '
+	dta d'               '
+LANGUAGE_LABEL
+	dta d'  POLSKI '
+	dta d'                '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                 Powrot                 '
+
+MENU_1_DATA_EN
+	dta d'         Gravity acceleration:          '
+	dta d'           '
+GRAVITY_LABEL_1
+	dta d'     MIGHTY       '*
+	dta d'           '
+	dta d'                                        '
+	dta d'            Level rotation:             '
+	dta d'               '
+ROTATION_LABEL_1
+	dta d'   ON     '
+	dta d'               '
+	dta d'                                        '
+	dta d'               Language:                '
+	dta d'           '
+LANGUAGE_LABEL_1
+	dta d'     ENGLISH      '
+	dta d'           '
+	dta d'                                        '
+	dta d'                                        '
+	dta d'                  Back                  '
 MAP_01_NAME
 		dta d'kr'*,b(1+64*3),d'pcewo w prawo'*
 		dta d'    I W LEWO    '*
@@ -479,6 +605,164 @@ MAP_02_NAME
 		dta d'51'
 MAP_NAME_LAST
 		dta b($9b)
+MAP_01_NAME_EN
+		dta d'kr'*,b(1+64*3),d'pcewo w prawo'*
+		dta d'    I W LEWO    '*
+MAP_01_NAME_END_EN
+		dta d'01'
+MAP_02_NAME_EN
+		dta d'   staro'*,b(10+64*3),d'ytne   '*
+		dta d'  KO'*,b(2+64*2),d'CIEPIECHO  '*
+		dta d'02'
+		dta d'sple'*,b(2+64*3),d'nia'*,b(11+64*3),d'a grota'*
+		dta d'  OTY'*,b(11+64*2),d'EGO KOTA  '*
+		dta d'03'
+		dta d' sawa woda alfa '*
+		dta d' SANTIAGO BELLA '*
+		dta d'04'
+		dta d' wydrapany loft '*
+		dta d'FERMENTACJI SERA'*
+		dta d'05'
+		dta d' dystopia sze'*,b(2+64*3),b(3+64*3),d' '*
+		dta d'DLA UMYTYCH LW'*,b(5+64*2),d'W'*
+		dta d'06'
+		dta d'komora kompresji'*
+		dta d'STAWONOG'*,b(5+64*2),d'W I KUR'*
+		dta d'07'
+		dta d'  miejsce bytu  '*
+		dta d'DWUMOLIBDENOWEGO'*
+		dta d'08'
+		dta d'swie'*,b(10+64*3),d'o wydr'*,b(4+64*3),b(10+64*3),d'ona'*
+		dta d'JAMA '*,b(10+64*2),d'UKA ZENONA'*
+		dta d'09'
+		dta d' przesi'*,b(4+64*3),d'kni'*,b(1+64*3),d'cie '*
+		dta d'  PONUREGO Z'*,b(11+64*2),d'A  '*
+		dta d'10'
+		dta d'tryskaj'*,b(4+64*3),d'ca jadem'*
+		dta d'KLITA CHRZCIELNA'*
+		dta d'11'
+		dta d' grzyby widelec '*
+		dta b(10+64*2),d'AR'*,b(5+64*2),d'WKA ROBOT V4'*
+		dta d'12'
+		dta d'podwodna piwnica'*
+		dta d'Z MI'*,b(1+64*2),d'SEM I PSAMI'*
+		dta d'13'
+		dta d'  krater z'*,b(11+64*3),d'a i  '*
+		dta d'SPIENIONEJ PIANY'*
+		dta d'14'
+		dta d'nisza schorza'*,b(11+64*3),d'ej'*
+		dta d' KOLONISTKI OLI '*
+		dta d'15'
+		dta d'izba sn'*,b(5+64*3),d'w du'*,b(10+64*3),d'ego'*
+		dta d'P'*,b(1+64*2),d'KATEGO SZATANA'*
+		dta d'16'
+		dta d'sprz'*,b(1+64*3),d'g kolimacji'*
+		dta d'NIKLOWO-KADMOWEJ'*
+		dta d'17'
+		dta d'   ponury k'*,b(4+64*3),d't   '*
+		dta d'ZAKICHANYCH CIA'*,b(11+64*2)
+		dta d'18'
+		dta d' jama przebicia '*
+		dta d'SYFU Z ZA'*,b(2+64*2),d'WIAT'*,b(5+64*2),d'W'*
+		dta d'19'
+		dta d' zw'*,b(1+64*3),d'glona budka '*
+		dta d'  PE'*,b(11+64*2),d'NA MITOZY  '*
+		dta d'20'
+		dta d' gibony strachu '*
+		dta d' TU JADA'*,b(11+64*2),d'Y '*,b(2+64*2),d'LUZ '*
+		dta d'21'
+		dta d' dom '*,b(10+64*3),d'ony brata '*
+		dta d' SIOSTRY TE'*,b(2+64*2),d'CIA '*
+		dta d'22'
+		dta d'podwodny bezkres'*
+		dta d'POLA '*,b(2+64*2),d'LEDZIOWEGO'*
+		dta d'23'
+		dta d' dwie zaropia'*,b(11+64*3),d'e '*
+		dta d'WN'*,b(1+64*2),d'TRZNO'*,b(2+64*2),d'CI DYNI'*
+		dta d'24'
+		dta b(2+64*3),d'luza numer trzy'*
+		dta d'ONGI'*,b(2+64*2),d' DZIA'*,b(11+64*2),d'AJ'*,b(4+64*2),d'CA'*
+		dta d'25'
+		dta d'   przedpok'*,b(5+64*3),d'j   '*
+		dta d'SNYCERSKICH PS'*,b(5+64*2),d'W'*
+		dta d'26'
+		dta d'zbiornik balsamu'*
+		dta d' Z DERMATOFIT'*,b(5+64*2),d'W '*
+		dta d'27'
+		dta d'miejsce poch'*,b(5+64*3),d'wku'*
+		dta d'OGORZA'*,b(11+64*2),d'EJ ESTERY'*
+		dta d'28'
+		dta d'  upiorny loft  '*
+		dta d'Z KRWAW'*,b(4+64*2),d' KAPLIC'*,b(4+64*2)
+		dta d'29'
+		dta d'  biblioteka z  '*
+		dta d'  UMAR'*,b(11+64*2),b(4+64*2),d' PRAS'*,b(4+64*2),d'  '*
+		dta d'30'
+		dta d' pop'*,b(1+64*3),d'kany strop '*
+		dta d' GROZI KOLAPSJ'*,b(4+64*2),d' '*
+		dta d'31'
+		dta d'  wiktoria'*,b(15+64*3),d'ski  '*
+		dta d'     RYGIEL     '*
+		dta d'32'
+		dta d'   tunele pod   '*
+		dta d' MIASTEM UWI'*,b(4+64*2),d'DU '*
+		dta d'33'
+		dta d'  przeci'*,b(4+64*3),d'g jak  '*
+		dta d'   Z KOSZMARU   '*
+		dta d'34'
+		dta d' bezkresne pola '*
+		dta d'    OGLEJONU    '*
+		dta d'35'
+		dta d'    jaskinia    '*
+		dta d'PONUREGO KINTARO'*
+		dta d'36'
+		dta d'     w'*,b(5+64*3),d'lka      '*
+		dta d'  DOBROWOLSKA   '*
+		dta d'37'
+		dta d'    prze'*,b(11+64*3),b(1+64*3),d'cz    '*
+		dta d'  MASZTALERSKA  '*
+		dta d'38'
+		dta d'    anomalia    '*
+		dta d'    RZYGULIN    '*
+		dta d'39'
+		dta d'      ch'*,b(5+64*3),d'r      '*
+		dta d'   BEMBNIST'*,b(5+64*2),d'W   '*
+		dta d'40'
+		dta d'     zau'*,b(11+64*3),d'ek     '*
+		dta d' Z'*,b(11+64*2),d'EJ TEKTONIKI '*
+		dta d'41'
+		dta d'  termy '*,b(10+64*3),d'ywego  '*
+		dta d'      BOGA      '*
+		dta d'42'
+		dta d' buda sanacyjna '*
+		dta d' NAD PRZEPA'*,b(2+64*2),d'CI'*,b(4+64*2),d' '*
+		dta d'43'
+		dta d'     podkop     '*
+		dta d' DR. KAMI'*,b(15+64*2),d'SKIEJ '*
+		dta d'44'
+		dta d'   paul morty   '*
+		dta d'KOHORTY GERIATRY'*
+		dta d'45'
+		dta d'ponure podziemne'*
+		dta d'OPACTWO PRZEMOCY'*
+		dta d'46'
+		dta d'   przepastne   '*
+		dta d'    BEZDRO'*,b(10+64*2),d'A    '*
+		dta d'47'
+		dta d' podziemny targ '*
+		dta d'Z RYBAMI I SEREM'*
+		dta d'48'
+		dta d'    kryj'*,b(5+64*3),d'wka    '*
+		dta d' KORUPCJONIST'*,b(5+64*2),d'W '*
+		dta d'49'
+		dta d'  zalany tunel  '*
+		dta d'   SZMUGLER'*,b(5+64*2),d'W   '*
+		dta d'50'
+		dta d' dwa anihilanty '*
+		dta d'ZNAMION KALECTWA'*
+		dta d'51'
+MAP_NAME_LAST_EN
+		dta b($9b)
 
 INSTRUCTION_DATA
 	dta d'bcdefghijklmnopqrstuvxyzabcdefghijklmnop'
@@ -505,135 +789,6 @@ INSTRUCTION_DATA_EN
 	dta d'bcdefghijklmnopqrstuvxyzabcdefghijklmnop'
 	dta d'abcdefghijklmnopqrstuvxyzabcdefghijklmno'
 	dta d'012345678901234567890123456789012ENGLISZ'
-
-LUDEK_DATA
-		dta b($62)
-		dta b($16)
-		dta b($38)
-		dta b($18)
-		dta b($70)
-		dta b($30)
-		dta b($70)
-		dta b($00)
-		
-		dta b($30)
-		dta b($1C)
-		dta b($18)
-		dta b($18)
-		dta b($10)
-		dta b($38)
-		dta b($18)
-		dta b($38)
-		
-		dta b($18)
-		dta b($18)
-		dta b($1C)
-		dta b($18)
-		dta b($10)
-		dta b($38)
-		dta b($18)
-		dta b($38)
-
-		dta b($0C)
-		dta b($30)
-		dta b($18)
-		dta b($18)
-		dta b($10)
-		dta b($38)
-		dta b($18)
-		dta b($38)
-
-AMYGDALA_DATA_5	; Miecz
-	dta b(0),b(192),b(160),b(84),b(44),b(24),b(52),b(2),b($64),b($74)
-
-AMYGDALA_DATA_6	; Pierscionek
-	dta b(0),b(60),b(24),b(52),b(82),b(64),b(66),b(52),b($a6),b($b6)
-
-AMYGDALA_DATA_7	; Robak
-	dta b(0),b(146),b(130),b(84),b(16),b(88),b(16),b(56),b($34),b($44)
-
-scr_head     .he 00 00 00 00 00 00 00 c1 00 00 00 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 42 00 03 04 05 06 87 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 00 08 09 0a 0b 0c 00 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 4d 00 0e 0f 10 d1 d2 00 93 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 00 00 d4 15 d4 00 00 00 00 00 00 00 00 00 00 00
- 	.he 00 00 00 00 00 16 17 18 99 9a 00 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 00 00 1b 5c 9d 00 00 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 00 00 1e 1f a0 00 00 00 00 00 00 00 00 00 00 00
-	.he 00 00 00 61+128 62+128 63+128 00+128 64+128 65+128 66+128 67+128 68+128 00 00 00 00 00 00 00 00
-	.he 00 00 00 00 69 6a 6b 6c 6d 6e 6f 79+128 00 00 00 00 00 00 00 00
-	.he 00 00 00 70+128 71+128 72+128 73+128 74+128 75+128 76+128 77+128 78+128 00 00 00 00 00 00 00 00
-
-MENU_0_DATA
-	dta d'               Graj                     '
-	dta d'                                        '
-	dta d'               Opcje                    '
-	dta d'                                        '
-	dta d'               Instrukcja               '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'               Wyjscie                  '
-
-MENU_0_DATA_EN
-	dta d'               Play                     '
-	dta d'                                        '
-	dta d'               Options                  '
-	dta d'                                        '
-	dta d'               Info                     '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'               Exit                     '
-
-MENU_1_DATA
-	dta d'      Przyspieszenie grawitacyjne:      '
-	dta d'           '
-GRAVITY_LABEL
-	dta d'     POTEZNE      '*
-	dta d'           '
-	dta d'                                        '
-	dta d'            Obrot pieczary:             '
-	dta d'               '
-ROTATION_LABEL
-	dta d' WLACZONY'
-	dta d'                '
-	dta d'                                        '
-	dta d'                 Jezyk:                 '
-	dta d'               '
-LANGUAGE_LABEL
-	dta d'  POLSKI '
-	dta d'                '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                 Powrot                 '
-
-MENU_1_DATA_EN
-	dta d'         Gravity acceleration:          '
-	dta d'           '
-GRAVITY_LABEL_1
-	dta d'     MIGHTY       '*
-	dta d'           '
-	dta d'                                        '
-	dta d'            Level rotation:             '
-	dta d'               '
-ROTATION_LABEL_1
-	dta d'   ON     '
-	dta d'               '
-	dta d'                                        '
-	dta d'               Language:                '
-	dta d'           '
-LANGUAGE_LABEL_1
-	dta d'     ENGLISH      '
-	dta d'           '
-	dta d'                                        '
-	dta d'                                        '
-	dta d'                  Back                  '
-		
 
 FONT_MAPPER
 		dta b(>FONT_SLOT_1)			; North

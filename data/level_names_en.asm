fcbb1432db8fcb13988db209591b1a7b59bc1283
mgr_inz_rafal <rchabowski@gmail.com>
2021-12-28 20:03:54 +0100

    Level names are stored under the OS

---
+++ data/level_names_en.asm
@@ -0,0 +1,158 @@
+MAP_01_NAME
+		dta d'ENGLISH NAME    '*
+		dta d'FOR FIRST CAVERN'*
+MAP_01_NAME_END
+		dta d'01'
+MAP_02_NAME
+		dta d'                '*
+		dta d'                '*
+		dta d'02'
+		dta d'                '*
+		dta d'                '*
+		dta d'03'
+		dta d'                '*
+		dta d'                '*
+		dta d'04'
+		dta d'                '*
+		dta d'                '*
+		dta d'05'
+		dta d'                '*
+		dta d'                '*
+		dta d'06'
+		dta d' MAPA           '*
+		dta d'    SEVENOVA    '*
+		dta d'07'
+		dta d'                '*
+		dta d'                '*
+		dta d'08'
+		dta d'                '*
+		dta d'                '*
+		dta d'09'
+		dta d'                '*
+		dta d'                '*
+		dta d'10'
+		dta d'                '*
+		dta d'                '*
+		dta d'11'
+		dta d'                '*
+		dta d'                '*
+		dta d'12'
+		dta d'                '*
+		dta d'                '*
+		dta d'13'
+		dta d'                '*
+		dta d'                '*
+		dta d'14'
+		dta d'                '*
+		dta d'                '*
+		dta d'15'
+		dta d'                '*
+		dta d'                '*
+		dta d'16'
+		dta d'                '*
+		dta d'                '*
+		dta d'17'
+		dta d'                '*
+		dta d'                '*
+		dta d'18'
+		dta d'                '*
+		dta d'                '*
+		dta d'19'
+		dta d'                '*
+		dta d'                '*
+		dta d'20'
+		dta d'                '*
+		dta d'                '*
+		dta d'21'
+		dta d'                '*
+		dta d'                '*
+		dta d'22'
+		dta d'                '*
+		dta d'                '*
+		dta d'23'
+		dta d'                '*
+		dta d'                '*
+		dta d'24'
+		dta d'                '*
+		dta d'                '*
+		dta d'25'
+		dta d'                '*
+		dta d'                '*
+		dta d'26'
+		dta d'                '*
+		dta d'                '*
+		dta d'27'
+		dta d'                '*
+		dta d'                '*
+		dta d'28'
+		dta d'                '*
+		dta d'                '*
+		dta d'29'
+		dta d'                '*
+		dta d'                '*
+		dta d'30'
+		dta d'                '*
+		dta d'                '*
+		dta d'31'
+		dta d'                '*
+		dta d'                '*
+		dta d'32'
+		dta d'                '*
+		dta d'                '*
+		dta d'33'
+		dta d'                '*
+		dta d'                '*
+		dta d'34'
+		dta d'                '*
+		dta d'                '*
+		dta d'35'
+		dta d'                '*
+		dta d'                '*
+		dta d'36'
+		dta d'                '*
+		dta d'                '*
+		dta d'37'
+		dta d'                '*
+		dta d'                '*
+		dta d'38'
+		dta d'                '*
+		dta d'                '*
+		dta d'39'
+		dta d'                '*
+		dta d'                '*
+		dta d'40'
+		dta d'                '*
+		dta d'                '*
+		dta d'41'
+		dta d'                '*
+		dta d'                '*
+		dta d'42'
+		dta d'                '*
+		dta d'                '*
+		dta d'43'
+		dta d'                '*
+		dta d'                '*
+		dta d'44'
+		dta d'                '*
+		dta d'                '*
+		dta d'45'
+		dta d'                '*
+		dta d'                '*
+		dta d'46'
+		dta d'                '*
+		dta d'                '*
+		dta d'47'
+		dta d'                '*
+		dta d'                '*
+		dta d'48'
+		dta d'                '*
+		dta d'                '*
+		dta d'49'
+		dta d'                '*
+		dta d'                '*
+		dta d'50'
+		dta d'ENGLISH NAME    '*
+		dta d' FOR LAST CAVERN'*
+		dta d'51'
+MAP_NAME_LAST
+		dta b($9b)
--- tensor.asm
+++ tensor.asm
@@ -48,7 +48,6 @@ CS_FADEIN		equ 1	  ; Credits are fading in
 CS_SHOW			equ	2	  ; Credits are being shown
 MAP_STORAGE		equ $d800 ; Maps stored under the OS
 FONTS_STORAGE	equ $c000 ; 4 fonts
-LAST_FONT_STORAGE equ $FFFA-1-1024 ; 5th font
 HI_SCORE_TABLE  equ MAPS_END_IN_STORAGE+1
 MENU_ITEM_OFFSET equ (40/2-12/2)
 MS_MAIN			equ 1
@@ -158,11 +157,14 @@ HEIGHT	= 30
 ; ---	BASIC switch OFF
 	org $2000\ mva #$ff portb\ rts\ ini $2000
 
-; ---	Load map data and move under OS
+; ---	Load some data into the $D800 region (second OS ROM block)
 	org $2000
 .rept MAPCOUNT #+1
 	ins "maps\v:1.map"
 .endr
+ENGLISH_LEVEL_NAMES equ *-$2000 + MAP_STORAGE		
+		ins "data\level_names_en.obx.kloc"
+ENGLISH_LEVEL_NAMES_END equ *-$2000 + MAP_STORAGE		
 MAPS_END equ *
 MAPS_END_IN_STORAGE equ MAPS_END + MAP_STORAGE - $2000
 
@@ -208,7 +210,7 @@ COPY_DONE
 	rts
 	ini COPY_UNDER_OS
 
-; ---	Load first 4 fonts and move under OS
+; ---	Load some data into the $C000 region (first OS ROM block)
 	org $2000
 CREDITS_FONT equ *-$2000 + FONTS_STORAGE		
 		ins "fonts\credits3.fnt.kloc"
@@ -225,7 +227,11 @@ DIGITS_FONT_END equ *-$2000 + FONTS_STORAGE
 GAME_FONT_2 equ *-$2000 + FONTS_STORAGE		
 		ins "fonts\fontek2.fnt.kloc"
 GAME_FONT_2_END equ *-$2000 + FONTS_STORAGE		
+POLISH_LEVEL_NAMES equ *-$2000 + FONTS_STORAGE		
+		ins "data\level_names.obx.kloc"
+POLISH_LEVEL_NAMES_END equ *-$2000 + FONTS_STORAGE		
 FONTS_END equ *
+
 
 COPY_UNDER_OS_FONTS
 	sei
@@ -442,321 +448,165 @@ LANGUAGE_LABEL_1
 	dta b(124),d'                                      ',b(124)
 	dta b(124),d'                                      ',b(124)
 	dta b(124),d'                 Back                 ',b(124)
+
+; TODO[RC]: Dedatatize this buffer
 MAP_01_NAME
-		dta d'kr'*,b(1+64*3),d'pcewo w prawo'*
-		dta d'    I W LEWO    '*
+		dta d'                '
+		dta d'                '
 MAP_01_NAME_END
-		dta d'01'
+		dta d'  '
 MAP_02_NAME
-		dta d'   staro'*,b(10+64*3),d'ytne   '*
-		dta d'  KO'*,b(2+64*2),d'CIEPIECHO  '*
-		dta d'02'
-		dta d'sple'*,b(2+64*3),d'nia'*,b(11+64*3),d'a grota'*
-		dta d'  OTY'*,b(11+64*2),d'EGO KOTA  '*
-		dta d'03'
-		dta d' sawa woda alfa '*
-		dta d' SANTIAGO BELLA '*
-		dta d'04'
-		dta d' wydrapany loft '*
-		dta d'FERMENTACJI SERA'*
-		dta d'05'
-		dta d' dystopia sze'*,b(2+64*3),b(3+64*3),d' '*
-		dta d'DLA UMYTYCH LW'*,b(5+64*2),d'W'*
-		dta d'06'
-		dta d'komora kompresji'*
-		dta d'STAWONOG'*,b(5+64*2),d'W I KUR'*
-		dta d'07'
-		dta d'  miejsce bytu  '*
-		dta d'DWUMOLIBDENOWEGO'*
-		dta d'08'
-		dta d'swie'*,b(10+64*3),d'o wydr'*,b(4+64*3),b(10+64*3),d'ona'*
-		dta d'JAMA '*,b(10+64*2),d'UKA ZENONA'*
-		dta d'09'
-		dta d' przesi'*,b(4+64*3),d'kni'*,b(1+64*3),d'cie '*
-		dta d'  PONUREGO Z'*,b(11+64*2),d'A  '*
-		dta d'10'
-		dta d'tryskaj'*,b(4+64*3),d'ca jadem'*
-		dta d'KLITA CHRZCIELNA'*
-		dta d'11'
-		dta d' grzyby widelec '*
-		dta b(10+64*2),d'AR'*,b(5+64*2),d'WKA ROBOT V4'*
-		dta d'12'
-		dta d'podwodna piwnica'*
-		dta d'Z MI'*,b(1+64*2),d'SEM I PSAMI'*
-		dta d'13'
-		dta d'  krater z'*,b(11+64*3),d'a i  '*
-		dta d'SPIENIONEJ PIANY'*
-		dta d'14'
-		dta d'nisza schorza'*,b(11+64*3),d'ej'*
-		dta d' KOLONISTKI OLI '*
-		dta d'15'
-		dta d'izba sn'*,b(5+64*3),d'w du'*,b(10+64*3),d'ego'*
-		dta d'P'*,b(1+64*2),d'KATEGO SZATANA'*
-		dta d'16'
-		dta d'sprz'*,b(1+64*3),d'g kolimacji'*
-		dta d'NIKLOWO-KADMOWEJ'*
-		dta d'17'
-		dta d'   ponury k'*,b(4+64*3),d't   '*
-		dta d'ZAKICHANYCH CIA'*,b(11+64*2)
-		dta d'18'
-		dta d' jama przebicia '*
-		dta d'SYFU Z ZA'*,b(2+64*2),d'WIAT'*,b(5+64*2),d'W'*
-		dta d'19'
-		dta d' zw'*,b(1+64*3),d'glona budka '*
-		dta d'  PE'*,b(11+64*2),d'NA MITOZY  '*
-		dta d'20'
-		dta d' gibony strachu '*
-		dta d' TU JADA'*,b(11+64*2),d'Y '*,b(2+64*2),d'LUZ '*
-		dta d'21'
-		dta d' dom '*,b(10+64*3),d'ony brata '*
-		dta d' SIOSTRY TE'*,b(2+64*2),d'CIA '*
-		dta d'22'
-		dta d'podwodny bezkres'*
-		dta d'POLA '*,b(2+64*2),d'LEDZIOWEGO'*
-		dta d'23'
-		dta d' dwie zaropia'*,b(11+64*3),d'e '*
-		dta d'WN'*,b(1+64*2),d'TRZNO'*,b(2+64*2),d'CI DYNI'*
-		dta d'24'
-		dta b(2+64*3),d'luza numer trzy'*
-		dta d'ONGI'*,b(2+64*2),d' DZIA'*,b(11+64*2),d'AJ'*,b(4+64*2),d'CA'*
-		dta d'25'
-		dta d'   przedpok'*,b(5+64*3),d'j   '*
-		dta d'SNYCERSKICH PS'*,b(5+64*2),d'W'*
-		dta d'26'
-		dta d'zbiornik balsamu'*
-		dta d' Z DERMATOFIT'*,b(5+64*2),d'W '*
-		dta d'27'
-		dta d'miejsce poch'*,b(5+64*3),d'wku'*
-		dta d'OGORZA'*,b(11+64*2),d'EJ ESTERY'*
-		dta d'28'
-		dta d'  upiorny loft  '*
-		dta d'Z KRWAW'*,b(4+64*2),d' KAPLIC'*,b(4+64*2)
-		dta d'29'
-		dta d'  biblioteka z  '*
-		dta d'  UMAR'*,b(11+64*2),b(4+64*2),d' PRAS'*,b(4+64*2),d'  '*
-		dta d'30'
-		dta d' pop'*,b(1+64*3),d'kany strop '*
-		dta d' GROZI KOLAPSJ'*,b(4+64*2),d' '*
-		dta d'31'
-		dta d'  wiktoria'*,b(15+64*3),d'ski  '*
-		dta d'     RYGIEL     '*
-		dta d'32'
-		dta d'   tunele pod   '*
-		dta d' MIASTEM UWI'*,b(4+64*2),d'DU '*
-		dta d'33'
-		dta d'  przeci'*,b(4+64*3),d'g jak  '*
-		dta d'   Z KOSZMARU   '*
-		dta d'34'
-		dta d' bezkresne pola '*
-		dta d'    OGLEJONU    '*
-		dta d'35'
-		dta d'    jaskinia    '*
-		dta d'PONUREGO KINTARO'*
-		dta d'36'
-		dta d'     w'*,b(5+64*3),d'lka      '*
-		dta d'  DOBROWOLSKA   '*
-		dta d'37'
-		dta d'    prze'*,b(11+64*3),b(1+64*3),d'cz    '*
-		dta d'  MASZTALERSKA  '*
-		dta d'38'
-		dta d'    anomalia    '*
-		dta d'    RZYGULIN    '*
-		dta d'39'
-		dta d'      ch'*,b(5+64*3),d'r      '*
-		dta d'   BEMBNIST'*,b(5+64*2),d'W   '*
-		dta d'40'
-		dta d'     zau'*,b(11+64*3),d'ek     '*
-		dta d' Z'*,b(11+64*2),d'EJ TEKTONIKI '*
-		dta d'41'
-		dta d'  termy '*,b(10+64*3),d'ywego  '*
-		dta d'      BOGA      '*
-		dta d'42'
-		dta d' buda sanacyjna '*
-		dta d' NAD PRZEPA'*,b(2+64*2),d'CI'*,b(4+64*2),d' '*
-		dta d'43'
-		dta d'     podkop     '*
-		dta d' DR. KAMI'*,b(15+64*2),d'SKIEJ '*
-		dta d'44'
-		dta d'   paul morty   '*
-		dta d'KOHORTY GERIATRY'*
-		dta d'45'
-		dta d'ponure podziemne'*
-		dta d'OPACTWO PRZEMOCY'*
-		dta d'46'
-		dta d'   przepastne   '*
-		dta d'    BEZDRO'*,b(10+64*2),d'A    '*
-		dta d'47'
-		dta d' podziemny targ '*
-		dta d'Z RYBAMI I SEREM'*
-		dta d'48'
-		dta d'    kryj'*,b(5+64*3),d'wka    '*
-		dta d' KORUPCJONIST'*,b(5+64*2),d'W '*
-		dta d'49'
-		dta d'  zalany tunel  '*
-		dta d'   SZMUGLER'*,b(5+64*2),d'W   '*
-		dta d'50'
-		dta d' dwa anihilanty '*
-		dta d'ZNAMION KALECTWA'*
-		dta d'51'
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
+		dta d'                '
+		dta d'                '
+		dta d'  '
 MAP_NAME_LAST
-		dta b($9b)
-MAP_01_NAME_EN
-		dta d'kr'*,b(1+64*3),d'pcewo w prawo'*
-		dta d'    I W LEWO    '*
-MAP_01_NAME_END_EN
-		dta d'01'
-MAP_02_NAME_EN
-		dta d'   staro'*,b(10+64*3),d'ytne   '*
-		dta d'  KO'*,b(2+64*2),d'CIEPIECHO  '*
-		dta d'02'
-		dta d'sple'*,b(2+64*3),d'nia'*,b(11+64*3),d'a grota'*
-		dta d'  OTY'*,b(11+64*2),d'EGO KOTA  '*
-		dta d'03'
-		dta d' sawa woda alfa '*
-		dta d' SANTIAGO BELLA '*
-		dta d'04'
-		dta d' wydrapany loft '*
-		dta d'FERMENTACJI SERA'*
-		dta d'05'
-		dta d' dystopia sze'*,b(2+64*3),b(3+64*3),d' '*
-		dta d'DLA UMYTYCH LW'*,b(5+64*2),d'W'*
-		dta d'06'
-		dta d'komora kompresji'*
-		dta d'STAWONOG'*,b(5+64*2),d'W I KUR'*
-		dta d'07'
-		dta d'  miejsce bytu  '*
-		dta d'DWUMOLIBDENOWEGO'*
-		dta d'08'
-		dta d'swie'*,b(10+64*3),d'o wydr'*,b(4+64*3),b(10+64*3),d'ona'*
-		dta d'JAMA '*,b(10+64*2),d'UKA ZENONA'*
-		dta d'09'
-		dta d' przesi'*,b(4+64*3),d'kni'*,b(1+64*3),d'cie '*
-		dta d'  PONUREGO Z'*,b(11+64*2),d'A  '*
-		dta d'10'
-		dta d'tryskaj'*,b(4+64*3),d'ca jadem'*
-		dta d'KLITA CHRZCIELNA'*
-		dta d'11'
-		dta d' grzyby widelec '*
-		dta b(10+64*2),d'AR'*,b(5+64*2),d'WKA ROBOT V4'*
-		dta d'12'
-		dta d'podwodna piwnica'*
-		dta d'Z MI'*,b(1+64*2),d'SEM I PSAMI'*
-		dta d'13'
-		dta d'  krater z'*,b(11+64*3),d'a i  '*
-		dta d'SPIENIONEJ PIANY'*
-		dta d'14'
-		dta d'nisza schorza'*,b(11+64*3),d'ej'*
-		dta d' KOLONISTKI OLI '*
-		dta d'15'
-		dta d'izba sn'*,b(5+64*3),d'w du'*,b(10+64*3),d'ego'*
-		dta d'P'*,b(1+64*2),d'KATEGO SZATANA'*
-		dta d'16'
-		dta d'sprz'*,b(1+64*3),d'g kolimacji'*
-		dta d'NIKLOWO-KADMOWEJ'*
-		dta d'17'
-		dta d'   ponury k'*,b(4+64*3),d't   '*
-		dta d'ZAKICHANYCH CIA'*,b(11+64*2)
-		dta d'18'
-		dta d' jama przebicia '*
-		dta d'SYFU Z ZA'*,b(2+64*2),d'WIAT'*,b(5+64*2),d'W'*
-		dta d'19'
-		dta d' zw'*,b(1+64*3),d'glona budka '*
-		dta d'  PE'*,b(11+64*2),d'NA MITOZY  '*
-		dta d'20'
-		dta d' gibony strachu '*
-		dta d' TU JADA'*,b(11+64*2),d'Y '*,b(2+64*2),d'LUZ '*
-		dta d'21'
-		dta d' dom '*,b(10+64*3),d'ony brata '*
-		dta d' SIOSTRY TE'*,b(2+64*2),d'CIA '*
-		dta d'22'
-		dta d'podwodny bezkres'*
-		dta d'POLA '*,b(2+64*2),d'LEDZIOWEGO'*
-		dta d'23'
-		dta d' dwie zaropia'*,b(11+64*3),d'e '*
-		dta d'WN'*,b(1+64*2),d'TRZNO'*,b(2+64*2),d'CI DYNI'*
-		dta d'24'
-		dta b(2+64*3),d'luza numer trzy'*
-		dta d'ONGI'*,b(2+64*2),d' DZIA'*,b(11+64*2),d'AJ'*,b(4+64*2),d'CA'*
-		dta d'25'
-		dta d'   przedpok'*,b(5+64*3),d'j   '*
-		dta d'SNYCERSKICH PS'*,b(5+64*2),d'W'*
-		dta d'26'
-		dta d'zbiornik balsamu'*
-		dta d' Z DERMATOFIT'*,b(5+64*2),d'W '*
-		dta d'27'
-		dta d'miejsce poch'*,b(5+64*3),d'wku'*
-		dta d'OGORZA'*,b(11+64*2),d'EJ ESTERY'*
-		dta d'28'
-		dta d'  upiorny loft  '*
-		dta d'Z KRWAW'*,b(4+64*2),d' KAPLIC'*,b(4+64*2)
-		dta d'29'
-		dta d'  biblioteka z  '*
-		dta d'  UMAR'*,b(11+64*2),b(4+64*2),d' PRAS'*,b(4+64*2),d'  '*
-		dta d'30'
-		dta d' pop'*,b(1+64*3),d'kany strop '*
-		dta d' GROZI KOLAPSJ'*,b(4+64*2),d' '*
-		dta d'31'
-		dta d'  wiktoria'*,b(15+64*3),d'ski  '*
-		dta d'     RYGIEL     '*
-		dta d'32'
-		dta d'   tunele pod   '*
-		dta d' MIASTEM UWI'*,b(4+64*2),d'DU '*
-		dta d'33'
-		dta d'  przeci'*,b(4+64*3),d'g jak  '*
-		dta d'   Z KOSZMARU   '*
-		dta d'34'
-		dta d' bezkresne pola '*
-		dta d'    OGLEJONU    '*
-		dta d'35'
-		dta d'    jaskinia    '*
-		dta d'PONUREGO KINTARO'*
-		dta d'36'
-		dta d'     w'*,b(5+64*3),d'lka      '*
-		dta d'  DOBROWOLSKA   '*
-		dta d'37'
-		dta d'    prze'*,b(11+64*3),b(1+64*3),d'cz    '*
-		dta d'  MASZTALERSKA  '*
-		dta d'38'
-		dta d'    anomalia    '*
-		dta d'    RZYGULIN    '*
-		dta d'39'
-		dta d'      ch'*,b(5+64*3),d'r      '*
-		dta d'   BEMBNIST'*,b(5+64*2),d'W   '*
-		dta d'40'
-		dta d'     zau'*,b(11+64*3),d'ek     '*
-		dta d' Z'*,b(11+64*2),d'EJ TEKTONIKI '*
-		dta d'41'
-		dta d'  termy '*,b(10+64*3),d'ywego  '*
-		dta d'      BOGA      '*
-		dta d'42'
-		dta d' buda sanacyjna '*
-		dta d' NAD PRZEPA'*,b(2+64*2),d'CI'*,b(4+64*2),d' '*
-		dta d'43'
-		dta d'     podkop     '*
-		dta d' DR. KAMI'*,b(15+64*2),d'SKIEJ '*
-		dta d'44'
-		dta d'   paul morty   '*
-		dta d'KOHORTY GERIATRY'*
-		dta d'45'
-		dta d'ponure podziemne'*
-		dta d'OPACTWO PRZEMOCY'*
-		dta d'46'
-		dta d'   przepastne   '*
-		dta d'    BEZDRO'*,b(10+64*2),d'A    '*
-		dta d'47'
-		dta d' podziemny targ '*
-		dta d'Z RYBAMI I SEREM'*
-		dta d'48'
-		dta d'    kryj'*,b(5+64*3),d'wka    '*
-		dta d' KORUPCJONIST'*,b(5+64*2),d'W '*
-		dta d'49'
-		dta d'  zalany tunel  '*
-		dta d'   SZMUGLER'*,b(5+64*2),d'W   '*
-		dta d'50'
-		dta d' dwa anihilanty '*
-		dta d'ZNAMION KALECTWA'*
-		dta d'51'
-MAP_NAME_LAST_EN
 		dta b($9b)
 
 INSTRUCTION_DATA
@@ -988,10 +838,10 @@ ff1	#if .word ptr0 <> #HI_SCORE_TABLE+640
 
 	mwa #TITLE_FONT ZX5_INPUT
 	mwa #FONT_SLOT_1 ZX5_OUTPUT
-	jsr load_font_from_storage
+	jsr decompress_data
 	mwa #CREDITS_FONT ZX5_INPUT
 	mwa #FONT_SLOT_2 ZX5_OUTPUT
-	jsr load_font_from_storage
+	jsr decompress_data
 	
 	lda #CS_FADEIN
 	sta credits_state
@@ -2516,10 +2366,10 @@ show_intermission
 		jsr sleep_for_short_time
 		mwa #DIGITS_FONT ZX5_INPUT
 		mwa #FONT_SLOT_1 ZX5_OUTPUT
-		jsr load_font_from_storage
+		jsr decompress_data
 		mwa #TITLE_FONT ZX5_INPUT
 		mwa #FONT_SLOT_2 ZX5_OUTPUT
-		jsr load_font_from_storage
+		jsr decompress_data
 		enable_antic
 
 		lda #0
@@ -2688,10 +2538,10 @@ init_game
 
 		mwa #GAME_FONT ZX5_INPUT
 		mwa #FONT_SLOT_1 ZX5_OUTPUT
-		jsr load_font_from_storage
+		jsr decompress_data
 		mwa #GAME_FONT_2 ZX5_INPUT
 		mwa #FONT_SLOT_2 ZX5_OUTPUT
-		jsr load_font_from_storage
+		jsr decompress_data
 
 		#if .byte first_run = #0
 			mva #1 first_run
@@ -3118,7 +2968,7 @@ show_margin
 
 		rts
 
-load_font_from_storage
+decompress_data
 		jsr os_gone
 		jsr unZX5
 		jsr os_back
@@ -3830,13 +3680,23 @@ enable_polish
 		rts
 
 show_level_selector
+		lda language
+		and #%00000001
+		beq sls_1
+		mwa #ENGLISH_LEVEL_NAMES ZX5_INPUT
+		mwa #MAP_01_NAME ZX5_OUTPUT
+		jmp sls_2
+sls_1	mwa #POLISH_LEVEL_NAMES ZX5_INPUT
+		mwa #MAP_01_NAME ZX5_OUTPUT
+sls_2	jsr decompress_data
+
 		lda #1 
 		sta dont_touch_menu
 		sta rmt_player_halt
 
 		mwa #TITLE_FONT ZX5_INPUT
 		mwa #FONT_SLOT_1 ZX5_OUTPUT
-		jsr load_font_from_storage
+		jsr decompress_data
 		lda >FONT_SLOT_1
 		sta CHBAS
 

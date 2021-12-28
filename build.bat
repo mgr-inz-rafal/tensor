8732e876acdc114979a1d8610afe1058296dfd9f
mgr_inz_rafal <rchabowski@gmail.com>
2021-12-28 20:24:13 +0100

    Compress map files

--- build.bat
+++ build.bat
@@ -1,9 +1,19 @@
 @ECHO OFF
 
-echo Compressing data...
+echo Compressing fonts...
 del fonts\*.kloc 2>NUL
 setlocal enabledelayedexpansion
 for %%f in (fonts\*.*) do (
+  set /p val=<%%f
+  tools\zx5.exe -f %%f %%f.kloc 2>&1 >NUL
+)
+echo DONE
+echo=
+
+echo Compressing maps...
+del maps\*.kloc 2>NUL
+setlocal enabledelayedexpansion
+for %%f in (maps\*.*) do (
   set /p val=<%%f
   tools\zx5.exe -f %%f %%f.kloc 2>&1 >NUL
 )

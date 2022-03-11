39e0b84678f881c87a658d35df2289c350130b6a
mgr_inz_rafal <rchabowski@gmail.com>
2022-03-11 20:34:00 +0100

    Prototype for summing the total score

--- tools/big_num/main.asm
+++ tools/big_num/main.asm
@@ -7,6 +7,7 @@ VCOUNT  equ	$D40B
 
 .zpvar  ptr0            .word
 .zpvar  ptr1            .word
+.zpvar  ptr2            .word
 
 LOOP
         lda #$ff
@@ -21,7 +22,7 @@ LOOP
         bne LOOP
 
         jsr SUM_RECORDS
-        ;jsr SUM_RECORDS_ADD_9999
+
         jsr SHOW_BUF
 
         jmp LOOP
@@ -36,13 +37,52 @@ SHB_0   lda NUMBUF,y
         bne SHB_0
         rts
 
+FIND_START_VALUE
+        ldx #0
+        mwa #HIGH_SCORE_TABLE ptr0
+        mwa #MUL9999_LUT ptr1
+        ldy #2
+FSV_AGAIN        
+        lda (ptr0),y
+        cmp #$ff
+        bne PROCESS_NEXT_ROW
+        inx 
+PROCESS_NEXT_ROW
+        adw ptr0 #(HIGH_SCORE_RECORD_END-HIGH_SCORE_RECORD_BEGIN)
+        nop
+        #if .word ptr0 = #HIGH_SCORE_TABLE_TRUE_END
+                rts
+        #end
+        jmp FSV_AGAIN
+
+APPLY_START_VALUE
+        mwa #MUL9999_LUT ptr0
+ASV_LOOP        
+        cpx #0
+        beq ASV_GOT_BASE
+        adw ptr0 #(MUL9999_LUT_ROW_END-MUL9999_LUT)
+        dex
+        jmp ASV_LOOP
+ASV_GOT_BASE
+        ldy #0
+ASV_LOOP2        
+        lda (ptr0),y
+        sta NUMBUF,y
+        iny
+        cpy #6
+        bne ASV_LOOP2
+        rts
+
 SUM_RECORDS
+        jsr FIND_START_VALUE
+        jsr APPLY_START_VALUE
+
         mwa #HIGH_SCORE_TABLE ptr0
 SUM_RECORDS_CONTINUE_WITH_NEXT_ROW
-;        ldy #2
-;        lda (ptr0),y
-;        cmp #$ff
-;        beq SUM_RECORDS_ADD_9999
+        ldy #2
+        lda (ptr0),y
+        cmp #$ff
+        beq SUM_RECORDS_FINISHED_ADDING_ROW
         ldy #0
         lda (ptr0),y
         sta ptr1+1
@@ -63,31 +103,17 @@ SUM_RECORDS_LOOP
         sta ptr1+1
         cld
         #if .word ptr1 == #0
-                jmp SUM_RECORDS_NEXT_ROW
+                jmp SUM_RECORDS_FINISHED_ADDING_ROW
         #end
         jmp SUM_RECORDS_LOOP
-SUM_RECORDS_NEXT_ROW
+
+SUM_RECORDS_FINISHED_ADDING_ROW
         adw ptr0 #(HIGH_SCORE_RECORD_END-HIGH_SCORE_RECORD_BEGIN)
-        nop        
+        nop
         #if .word ptr0 = #HIGH_SCORE_TABLE_TRUE_END
-SUM_RECORDS_EXIT
                 rts
         #end
         jmp SUM_RECORDS_CONTINUE_WITH_NEXT_ROW
-SUM_RECORDS_ADD_9999
-        ldy #1
-        lda NUMBUF,y
-        add #1
-        sta NUMBUF,y
-        cmp #26-16
-        bne SUM_RECORDS_EXIT
-        lda #0
-        sta NUMBUF,y
-        dey
-        lda NUMBUF,y
-        add #1
-        sta NUMBUF,y
-        rts
 
 INCREMENT
         ldy #5
@@ -97,7 +123,7 @@ INCREMENT_LOOP
         bne INCREMENT_EXIT
         dey
         jmp INCREMENT_LOOP
-INCREMENT_EXIT        
+INCREMENT_EXIT
         rts
 
 INCREMENT_NUM
@@ -108,7 +134,7 @@ INCREMENT_NUM
         beq INCREMENT_NUM_OVERFLOW
         ldx #0
         rts
-INCREMENT_NUM_OVERFLOW        
+INCREMENT_NUM_OVERFLOW
         lda #0
         sta NUMBUF,y
         ldx #1
@@ -140,11 +166,70 @@ NUMBUF
         dta b(0)
         dta b(0)
 
+MUL9999_LUT
+        dta b(0),b(0),b(0),b(0),b(0),b(0)  ; *0
+MUL9999_LUT_ROW_END        
+        dta b(0),b(0),b(9),b(9),b(9),b(9)  ; *1
+        dta b(0),b(1),b(9),b(9),b(9),b(8)  ; *2
+        dta b(0),b(2),b(9),b(9),b(9),b(7)  ; *3
+        dta b(0),b(3),b(9),b(9),b(9),b(6)  ; *4
+        dta b(0),b(4),b(9),b(9),b(9),b(5)  ; *5
+        dta b(0),b(5),b(9),b(9),b(9),b(4)  ; *6
+        dta b(0),b(6),b(9),b(9),b(9),b(3)  ; *7
+        dta b(0),b(7),b(9),b(9),b(9),b(2)  ; *8
+        dta b(0),b(8),b(9),b(9),b(9),b(1)  ; *9
+        dta b(0),b(9),b(9),b(9),b(9),b(0)  ; *10
+        dta b(1),b(0),b(9),b(9),b(8),b(9)  ; *11
+        dta b(1),b(1),b(9),b(9),b(8),b(8)  ; *12
+        dta b(1),b(2),b(9),b(9),b(8),b(7)  ; *13
+        dta b(1),b(3),b(9),b(9),b(8),b(6)  ; *14
+        dta b(1),b(4),b(9),b(9),b(8),b(5)  ; *15
+        dta b(1),b(5),b(9),b(9),b(8),b(4)  ; *16
+        dta b(1),b(6),b(9),b(9),b(8),b(3)  ; *17
+        dta b(1),b(7),b(9),b(9),b(8),b(2)  ; *18
+        dta b(1),b(8),b(9),b(9),b(8),b(1)  ; *19
+        dta b(1),b(9),b(9),b(9),b(8),b(0)  ; *20
+        dta b(2),b(0),b(9),b(9),b(7),b(9)  ; *21
+        dta b(2),b(1),b(9),b(9),b(7),b(8)  ; *22
+        dta b(2),b(2),b(9),b(9),b(7),b(7)  ; *23
+        dta b(2),b(3),b(9),b(9),b(7),b(6)  ; *24
+        dta b(2),b(4),b(9),b(9),b(7),b(5)  ; *25
+        dta b(2),b(5),b(9),b(9),b(7),b(4)  ; *26
+        dta b(2),b(6),b(9),b(9),b(7),b(3)  ; *27
+        dta b(2),b(7),b(9),b(9),b(7),b(2)  ; *28
+        dta b(2),b(8),b(9),b(9),b(7),b(1)  ; *29
+        dta b(2),b(9),b(9),b(9),b(7),b(0)  ; *30
+        dta b(3),b(0),b(9),b(9),b(6),b(9)  ; *31
+        dta b(3),b(1),b(9),b(9),b(6),b(8)  ; *32
+        dta b(3),b(2),b(9),b(9),b(6),b(7)  ; *33
+        dta b(3),b(3),b(9),b(9),b(6),b(6)  ; *34
+        dta b(3),b(4),b(9),b(9),b(6),b(5)  ; *35
+        dta b(3),b(5),b(9),b(9),b(6),b(4)  ; *36
+        dta b(3),b(6),b(9),b(9),b(6),b(3)  ; *37
+        dta b(3),b(7),b(9),b(9),b(6),b(2)  ; *38
+        dta b(3),b(8),b(9),b(9),b(6),b(1)  ; *39
+        dta b(3),b(9),b(9),b(9),b(6),b(0)  ; *40
+        dta b(4),b(0),b(9),b(9),b(5),b(9)  ; *41
+        dta b(4),b(1),b(9),b(9),b(5),b(8)  ; *42
+        dta b(4),b(2),b(9),b(9),b(5),b(7)  ; *43
+        dta b(4),b(3),b(9),b(9),b(5),b(6)  ; *44
+        dta b(4),b(4),b(9),b(9),b(5),b(5)  ; *45
+        dta b(4),b(5),b(9),b(9),b(5),b(4)  ; *46
+        dta b(4),b(6),b(9),b(9),b(5),b(3)  ; *47
+        dta b(4),b(7),b(9),b(9),b(5),b(2)  ; *48
+        dta b(4),b(8),b(9),b(9),b(5),b(1)  ; *49
+        dta b(4),b(9),b(9),b(9),b(5),b(0)  ; *50
+        dta b(5),b(0),b(9),b(9),b(4),b(9)  ; *51
+        dta b(5),b(1),b(9),b(9),b(4),b(8)  ; *52
+        dta b(5),b(2),b(9),b(9),b(4),b(7)  ; *53
+        dta b(5),b(3),b(9),b(9),b(4),b(6)  ; *54
+
+
 HIGH_SCORE_TABLE	; Can be moved under OS
 HIGH_SCORE_RECORD_BEGIN
-                   dta b($00),b($01),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
+                   dta b($00),b($01),b($aa),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
 HIGH_SCORE_RECORD_END
-                   dta b($00),b($02),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
+                   dta b($00),b($02),b($aa),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
@@ -196,7 +281,7 @@ HIGH_SCORE_RECORD_END
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
-                   dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
+                   dta b($12),b($34),b($aa),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
 HIGH_SCORE_TABLE_TRUE_END
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
@@ -209,4 +294,3 @@ HIGH_SCORE_TABLE_TRUE_END
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
                    dta b($99),b($99),b($ff),b('j'),b('e'),b('b'),b('a'),b('c'),b(' '),b('p'),b('i'),b('s')
 HIGH_SCORE_TABLE_END
-        
